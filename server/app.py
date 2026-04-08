499e3f26031d863bd1a6154710ad8d474107a92c
shree_man <shreeyasanj04@gmail.com>
2026-04-08 22:13:04 +0530

    Create app.py

---
+++ server/app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from env import SupportEnv
+
+app = FastAPI()
+
+env = SupportEnv()
+
+class Action(BaseModel):
+    response: str
+
+@app.post("/reset")
+def reset():
+    obs = env.reset()
+    return obs.dict()
+
+@app.post("/step")
+def step(action: Action):
+    obs, reward, done, info = env.step(action)
+    return {
+        "observation": obs.dict(),
+        "reward": reward,
+        "done": done,
+        "info": info
+    }
+
+@app.get("/state")
+def state():
+    return env.state()
