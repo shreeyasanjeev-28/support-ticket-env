af99e12817c24ae2d1675880e2732897aad1d032
shree_man <shreeyasanj04@gmail.com>
2026-04-10 20:30:21 +0530

    Update grader.py

--- grader.py
+++ grader.py
@@ -3,19 +3,19 @@ def grade_response(task_type, response):
 
     if task_type == "easy":
         if "track" in response or "order" in response:
-            return 0.8
+            return 0.75
         else:
-            return 0.2
+            return 0.25
 
     elif task_type == "medium":
         if "refund" in response or "replacement" in response:
-            return 0.85
+            return 0.8
         else:
-            return 0.3
+            return 0.35
 
     elif task_type == "hard":
         if "refund" in response or "escalate" in response:
-            return 0.9
+            return 0.85
         else:
             return 0.4
 
