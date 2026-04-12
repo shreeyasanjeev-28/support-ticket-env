d207796a3940049e42f24b66441fe81122085b68
shree_man <shreeyasanj04@gmail.com>
2026-04-12 20:33:23 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,5 +1,17 @@
 tasks = [
-    {"type": "easy", "ticket": "Where is my order?"},
-    {"type": "medium", "ticket": "I received a damaged product"},
-    {"type": "hard", "ticket": "I was charged twice for my order"}
+    {
+        "type": "easy",
+        "ticket": "Where is my order?",
+        "grader": "grade_response"
+    },
+    {
+        "type": "medium",
+        "ticket": "I received a damaged product",
+        "grader": "grade_response"
+    },
+    {
+        "type": "hard",
+        "ticket": "I was charged twice for my order",
+        "grader": "grade_response"
+    }
 ]
